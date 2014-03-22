3953150f1f80af87a293a2645155824c5aee8f2f
Pete Smith <roysvork@gmail.com>
2014-03-22 15:40:40 +0000

    Implemented in-memory event store

--- Codurance/Events/InMemoryEventStore.cs
+++ Codurance/Events/InMemoryEventStore.cs
@@ -1,22 +1,31 @@
 ﻿namespace Codurance.Events
 {
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class InMemoryEventStore : IEventStore
     {
+        private readonly ConcurrentQueue<IEvent> events;
+
+        public InMemoryEventStore()
+        {
+            this.events = new ConcurrentQueue<IEvent>();
+        }
+
         public void Publish(IEvent postEvent)
         {
-            throw new System.NotImplementedException();
+            events.Enqueue(postEvent);
         }
 
         public IEnumerable<FollowEvent> GetFollowEvents(string issuingUsername)
         {
-            throw new System.NotImplementedException();
+            return events.Where(o => o is FollowEvent).Cast<FollowEvent>().Where(o => o.IssuingUsername == issuingUsername);
         }
 
         public IEnumerable<PostEvent> GetPostEvents(string[] issuingUsernames)
         {
-            throw new System.NotImplementedException();
+            return events.Where(o => o is PostEvent).Cast<PostEvent>().Where(o => issuingUsernames.Contains(o.IssuingUsername));
         }
     }
 }

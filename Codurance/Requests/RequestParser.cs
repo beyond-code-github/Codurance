4be8de235c2ffc6407abb1d97d0a5637fa79c023
Pete Smith <roysvork@gmail.com>
2014-03-22 12:16:53 +0000

    Refactored request parser

--- Codurance/Requests/RequestParser.cs
+++ Codurance/Requests/RequestParser.cs
@@ -6,22 +6,37 @@
         {
             var parts = input.Split(' ');
 
-            if (parts.Length == 1)
+            if (IsReadRequest(parts))
             {
                 return new ReadRequest(parts[0]);
             }
 
-            if (parts.Length == 2)
+            if (IsWallRequest(parts))
             {
                 return new WallRequest(parts[0]);
             }
 
-            if (parts[1] == "->")
+            if (IsPostRequest(parts))
             {
                 return new PostRequest(parts[2], parts[0]);
             }
 
             return new FollowRequest(parts[0], parts[2]);
+        }
+
+        private static bool IsPostRequest(string[] parts)
+        {
+            return parts[1] == "->";
+        }
+
+        private static bool IsWallRequest(string[] parts)
+        {
+            return parts.Length == 2;
+        }
+
+        private static bool IsReadRequest(string[] parts)
+        {
+            return parts.Length == 1;
         }
     }
 }

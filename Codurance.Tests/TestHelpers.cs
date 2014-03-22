e3d15309f3da3ef0455b686f23c863f176d531d2
Pete Smith <roysvork@gmail.com>
2014-03-22 15:06:30 +0000

    Added methods to test helpers to generate a random number of follow or post events

--- Codurance.Tests/TestHelpers.cs
+++ Codurance.Tests/TestHelpers.cs
@@ -1,9 +1,15 @@
 ﻿namespace Codurance.Tests
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Codurance.Events;
 
     public static class TestHelpers
     {
+        public static Random gen = new Random();
+
         public static string RandomString()
         {
             return Guid.NewGuid().ToString();
@@ -12,9 +18,22 @@
         public static DateTime RandomDateTime()
         {
             var start = new DateTime(1995, 1, 1);
-            var gen = new Random();
             var range = ((DateTime.Today - start)).Days;
             return start.AddDays(gen.Next(range));
+        }
+
+        public static IEnumerable<FollowEvent> RandomFollowEvents(string username)
+        {
+            return
+                Enumerable.Range(0, gen.Next(1, 5))
+                    .Select(o => new FollowEvent(username, RandomString(), RandomDateTime()));
+        }
+
+        public static IEnumerable<PostEvent> RandomPostEvents()
+        {
+            return
+                Enumerable.Range(0, gen.Next(1, 5))
+                    .Select(o => new PostEvent(RandomString(), RandomString(), RandomDateTime()));
         }
     }
 }

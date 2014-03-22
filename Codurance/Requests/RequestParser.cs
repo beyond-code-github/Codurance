4f0580204e1d1bc55bfec77692b0576dab6a506e
Pete Smith <roysvork@gmail.com>
2014-03-22 12:13:18 +0000

    Implemented request parser

--- Codurance/Requests/RequestParser.cs
+++ Codurance/Requests/RequestParser.cs
@@ -4,7 +4,24 @@
     {
         public IRequest Parse(string input)
         {
-            throw new System.NotImplementedException();
+            var parts = input.Split(' ');
+
+            if (parts.Length == 1)
+            {
+                return new ReadRequest(parts[0]);
+            }
+
+            if (parts.Length == 2)
+            {
+                return new WallRequest(parts[0]);
+            }
+
+            if (parts[1] == "->")
+            {
+                return new PostRequest(parts[2], parts[0]);
+            }
+
+            return new FollowRequest(parts[0], parts[2]);
         }
     }
 }
